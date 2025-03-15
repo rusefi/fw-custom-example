802d02c9a2cba079b04c4f44359992b33eaa5179
GitHub git update Action <action@github.com>
2025-03-15 00:29:00 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_XYZ.h
+++ generated/controllers/generated/rusefi_generated_XYZ.h
@@ -1882,7 +1882,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.03.14.XYZ.3813386904"
+#define TS_SIGNATURE "rusEFI main.2025.03.15.XYZ.3813386904"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
