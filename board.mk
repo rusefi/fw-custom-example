5a9a449634a3296b25defb21478497cac3bdcc5f
rusefillc <rusefillc@gmail.com>
2025-08-12 16:21:14 -0400

    https://github.com/rusefi/rusefi/issues/8426

--- board.mk
+++ board.mk
@@ -12,3 +12,10 @@ DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+
+# EGT chip
+#un-comment to enable
+#DDEFS += -DEFI_MAX_31855=TRUE
+
+#see main repo for details on this any many other optional subsystems. We have too many, one has to choose what fits into his choice of stm32
+#DDEFS += -DEFI_ONBOARD_MEMS=TRUE
