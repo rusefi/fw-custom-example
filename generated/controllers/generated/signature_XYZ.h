035eb101691953d224850500fbe986ae368d485a
rusefillc <rusefillc@gmail.com>
2025-08-12 13:45:17 -0400

    helping Windows

--- generated/controllers/generated/signature_XYZ.h
+++
@@ -1,6 +0,0 @@
-//
-// CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2290350751
-#define TS_SIGNATURE "rusEFI main.2025.06.22.XYZ.2290350751"
