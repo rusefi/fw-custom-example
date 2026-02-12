e77580795e72b5551b2c986c080a74d64b046b68
rusefillc <rusefillc@gmail.com>
2026-02-12 08:45:53 -0500

    board.mk

---
+++ boards/board1/board_configuration.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "board_overrides.h"
+
+Gpio getCommsLedPin() {
+	return Gpio::Unassigned;
+}
+
+Gpio getRunningLedPin() {
+	return Gpio::Unassigned;
+}
+
+Gpio getWarningLedPin() {
+	return Gpio::Unassigned;
+}
+
+void setup_custom_board_overrides() {
+
+}
