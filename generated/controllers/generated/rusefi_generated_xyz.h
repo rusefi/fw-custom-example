130dc24b04dd010c982023d89d50499575545fad
GitHub git update Action <action@github.com>
2026-06-16 01:20:01 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_xyz.h
+++ generated/controllers/generated/rusefi_generated_xyz.h
@@ -106,6 +106,7 @@
 #define bench_mode_e_BENCH_AUXOUT5 25
 #define bench_mode_e_BENCH_AUXOUT6 26
 #define bench_mode_e_BENCH_AUXOUT7 27
+#define bench_mode_e_BENCH_BOOST_VALVE 43
 #define bench_mode_e_BENCH_CANCEL 15
 #define bench_mode_e_BENCH_CHECK_ENGINE_LIGHT 7
 #define bench_mode_e_BENCH_FAN_RELAY 4
@@ -307,6 +308,7 @@
 #define CLT_UPPER_VALIDATION_LIMIT 250
 #define CMD_AC_RELAY_BENCH "acrelaybench"
 #define CMD_ALTERNATOR_PIN "set_alternator_pin"
+#define CMD_BOOST_BENCH "boostbench"
 #define CMD_BOOST_PIN "set_boost_pin"
 #define CMD_BURNCONFIG "burnconfig"
 #define CMD_DATE "date"
@@ -1497,6 +1499,8 @@
 #define pin_output_mode_e_OM_OPENDRAIN 2
 #define pin_output_mode_e_OM_OPENDRAIN_INVERTED 3
 #define poke false
+#define PPS_FAIL_INDICATOR "Pedal error"
+#define PPS_OK_INDICATOR "Pedal OK"
 #define ppsExpAverageAlpha_NAME "Accelerator Exp Average"
 #define PRIMARY_CAN_NAME "Primary CAN"
 #define PRIME_CURVE_COUNT 8
@@ -1574,7 +1578,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 970118928
+#define SIGNATURE_HASH 3720446591
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2066,6 +2070,7 @@
 #define ts_show_gppwm_2 true
 #define ts_show_gppwm_3 true
 #define ts_show_gppwm_4 true
+#define ts_show_grab_tps true
 #define ts_show_hardware_simulator true
 #define ts_show_hbridge_function true
 #define ts_show_hd true
@@ -2190,7 +2195,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.06.15.xyz.970118928"
+#define TS_SIGNATURE "rusEFI main.2026.06.16.xyz.3720446591"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_xyz.h
+++ generated/controllers/generated/signature_xyz.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 970118928
-#define TS_SIGNATURE "rusEFI main.2026.06.15.xyz.970118928"
+#define SIGNATURE_HASH 3720446591
+#define TS_SIGNATURE "rusEFI main.2026.06.16.xyz.3720446591"
