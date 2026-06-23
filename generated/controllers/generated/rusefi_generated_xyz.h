f78a446bc60a515189d4cb998c981ae345de39b3
GitHub git update Action <action@github.com>
2026-06-23 01:04:37 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_xyz.h
+++ generated/controllers/generated/rusefi_generated_xyz.h
@@ -2213,7 +2213,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.06.22.xyz.4053871084"
+#define TS_SIGNATURE "rusEFI main.2026.06.23.xyz.4053871084"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
