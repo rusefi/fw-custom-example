9c6bf9ebcbf9a1b461ca2ede4a2b912a5bdf2ce7
rusefi llc <rusefillc@gmail.com>
2026-09-22 11:32:40 -0400

    usability

--- bin/_init.sh
+++ bin/_init.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# jump to repo root so this script works regardless of invocation directory
+cd "$(dirname "$0")/.."
 git submodule update --init
+cd ext/rusefi
+git submodule update --init java_console/peak-can-basic java_console/luaformatter
